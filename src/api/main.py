c1fd469b0d41008e70fe2e1fc887e4de6d216702
vishallmaurya <vishallmaurya210@gmail.com>
2025-04-10 17:51:59 +0000

    Cleaned repo - removed unwanted files

--- src/api/main.py
+++ src/api/main.py
@@ -1,5 +1,9 @@
-from predictor import app
 import uvicorn
+from src.api.predictor import app
+import sys
+import os 
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=10000)
