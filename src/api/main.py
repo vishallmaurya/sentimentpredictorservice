752849e5a1eb752944a9700b9ae0061515fe31f7
vishallmaurya <vishallmaurya210@gmail.com>
2025-10-03 10:40:54 +0530

    updates

--- src/api/main.py
+++ src/api/main.py
@@ -6,4 +6,5 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
 
 if __name__ == "__main__":
+    print("running")
     uvicorn.run(app, host="0.0.0.0", port=10000)
